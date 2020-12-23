f9498b00903ba5ab4912da9df0f113231aba028c
= <=>
2020-12-23 09:10:57 -0300

    test: should be able to create a new appointment

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,25 @@
-test('sum two number', () => {
-  expect(1 + 2).toBe(3);
+import 'reflect-metadata';
+
+import CreateAppointmentService from './CreateAppointmentService';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '312313e12',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('312313e12');
+  });
+
+  // it('should not be able to create two appointment on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });

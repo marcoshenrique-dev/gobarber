2707aef26b49e88ff6f3199eec2484b4d33fb8e6
= <=>
2020-12-28 15:34:21 -0300

    test: should be able to recover the password using the email

---
+++ src/modules/users/services/SendForgotPasswordEmailService.spec.ts
@@ -0,0 +1,32 @@
+import 'reflect-metadata';
+
+import AppError from '@shared/errors/AppError';
+import FakeMailProvider from '@shared/container/providers/MailProvider/fakes/FakeMailProvider';
+import SendForgotPasswordEmailService from './SendForgotPasswordEmailService';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+
+describe('SendForgotPasswordEmail', () => {
+  it('should be able to recover the password using the email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeMailProvider = new FakeMailProvider();
+
+    const sendMail = jest.spyOn(fakeMailProvider, 'sendMail');
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService(
+      fakeUsersRepository,
+      fakeMailProvider,
+    );
+
+    await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: 'r2r8ujijiji',
+    });
+
+    await sendForgotPasswordEmail.execute({
+      email: 'johndoe@gmail.com',
+    });
+
+    expect(sendMail).toHaveBeenCalled();
+  });
+});

d4c3c0b2138b2093d47f6c7762ade16a90540c08
= <=>
2020-12-28 21:35:53 -0300

    feat: removendo bug com hash provider

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -3,13 +3,18 @@ import { Request, Response } from 'express';
 import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
 
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import BCryptHashProvider from '@modules/users/providers/HashProvider/implementations/BCryptHashProvider';
 
 export default class SessionsController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { email, password } = request.body;
 
     const usersRepository = new UsersRepository();
-    const authenticateUser = new AuthenticateUserService(usersRepository);
+    const hashProvider = new BCryptHashProvider();
+    const authenticateUser = new AuthenticateUserService(
+      usersRepository,
+      hashProvider,
+    );
 
     const { user, token } = await authenticateUser.execute({
       email,
